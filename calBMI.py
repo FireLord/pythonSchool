010f7da244386a131a73a0e6a93e4ad37712be3e
Aman Kumar <firelord.xda@lineageos.org>
2018-08-22 17:47:09 +0530

    home: homework 1

---
+++ calBMI.py
@@ -0,0 +1,5 @@
+# to calcu;ate BMI = kg/m square
+weight_in_kg=float(input("Enter weight in kg:"))
+height_int_meter=float(input("Enter height in meters:"))
+bmi=weight_in_kg/(height_int_meter*height_int_meter)
+print("BMI is :",bmi)
