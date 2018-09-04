41cee5dc12e0954f1525e8595c27bc1e4257c3f5
Aman Kumar <firelord.xda@lineageos.org>
2018-09-04 21:44:39 +0530

    home: practice pg. 133-141

---
+++ oddOrEven.py
@@ -0,0 +1,6 @@
+# Program that takes a num and checks whether the given num is odd or even.
+num=int(input("Enter a number:"))
+if (num%2==0):
+    print(num, "is even number")
+else:
+    print(num,"is odd number")
