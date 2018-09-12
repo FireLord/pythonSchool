6eed0d6e0cb6233b2c6bacf02dd3c02dc279b0fc
FireLord1 <firelord.xda@gmail.com>
2018-09-12 22:23:13 +0530

    school: correct leapyear function for ques
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- leapYear.py
+++ leapYear.py
@@ -1,7 +1,5 @@
 year=int(input("Enter the year:"))
-if (year%4==0):
-    print("Its a leap year")
-elif ((year%100==0) and (year%400==0)):
+if (year%4==0 or (year%100==0 and year%400==0)):
     print("Its a leap year")
 else:
     print("Its not a leap year")
