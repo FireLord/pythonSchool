0ccf9c90a05f6242ea4d0938f3177b1624eb4f7c
FireLord <firelord.xda@aospextended.com>
2018-11-03 15:30:25 +0530

    home: Fix rows in pattern
    Signed-off-by: FireLord <firelord.xda@aospextended.com>

--- ch4/pyramid.py
+++ ch4/pyramid.py
@@ -2,7 +2,7 @@ num=int(input("Enter number of rows:"))
 for i in range(0,num):
     for j in range(0,num-i-1):
         print(end=" ")
-    for j in range(1,i+1):
+    for j in range(0,i+1):
         print("*",end=" ")
     print()
 
